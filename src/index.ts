8155bcb418746d9974b09372c392b180d07653e2
Adam Aiken <adamwtz@icloud.com>
2023-06-09 07:37:36 +0000

    chore: auto merge

--- src/index.ts
+++ src/index.ts
@@ -17,4 +17,4 @@ const pathsBFS = getAllGraphPathsBFS(adjacencyMap, 'A', 'H');
 pathsBFS.map((path) => console.log(`The path is ${path} by BFS`));
 
 const shortestPathBFS = getShortestPathBFS(adjacencyMap, 'A', 'H');
-console.log(`111The shortest path is ${shortestPathBFS} by BFS`);
+console.log(`The shortest path is ${shortestPathBFS} by BFS`);
